47a855a1b7adf77788de32be0db602282589ccd9
sakshamsuryawanshi21 <sakshamsuryawansh194@gmail.com>
2025-09-03 13:11:07 +0530

    UPDATED RESUME

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -40,7 +40,7 @@ const Header = ({isDarkMode}) => {
         <motion.a initial={{ y:-20,opacity:0 }}
   whileInView={{ y:0, opacity:1 }}
   transition={{duration:0.6 , delay:0.3}}
-         href="/SAKSHAMSURYAWANSHI-AP_RESUME.pdf"
+         href="/SakshamSuryawanshi_SDE.pdf"
   target="_blank"
   rel="noopener noreferrer"
   className="px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black"
